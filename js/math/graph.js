e188781b5f577c0156a079659cfec4ceca86df0c
Chris Mechi <kristoferoyemechi.onowu@rosbank.ru>
2023-12-15 19:51:14 +0300

    Old-Interface-Creation-With-Button-Click-To-Add-Remove-Clear-Points-And-Segments

--- js/math/graph.js
+++ js/math/graph.js
@@ -4,6 +4,65 @@ class Graph {
         this.segments = segments;
     }
 
+    addPoint(point) {
+        this.points.push(point);
+    }
+
+    containsPoint(point) {
+        return this.points.find((p) => p.equals(point) )
+    }
+
+    tryAddPoint(point) {
+        if(!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true
+        }
+        return false;
+    }
+    
+    removePoint(point) {
+        const segments = this.getSegmentsWithPoint(point);
+        for (const segment of segments) {
+            this.removeSegment(segment)
+        }
+        return this.points.splice(this.points.indexOf(point, 1));
+    }
+
+    addSegment(segment) {
+        this.segments.push(segment);
+    }
+
+    containsSegment(segment) {
+        return this.segments.find((seg) => seg.equals(segment));
+    }
+
+    getSegmentsWithPoint(point) {
+        const segments = [];
+        for (const segment of this.segments) {
+            if (segment.includes(point)) {
+                segments.push(point);
+            }
+        }
+        return segments;
+    }
+
+    tryAddSegment(segment) {
+        if (!this.containsSegment(segment) && !segment.p1.equals(segment.p2)) {
+            this.addSegment(segment)
+            return true;
+        }
+        return false;
+    }
+
+    removeSegment(segment) {
+        this.segments.splice(this.segments.indexOf(segment, 1));
+    }
+
+    clearAllPointsAndSegments() {
+        this.points.length = 0;
+        this.segments.length = 0;
+    }
+
     draw(ctx) {
         for (const segment of this.segments) {
             segment.draw(ctx);
--- js/math/primitives/segment.js
+++ js/math/primitives/segment.js
@@ -1,7 +1,15 @@
 class Segment {
     constructor(p1, p2) {
         this.p1 = p1;
-        this.p2 = p2
+        this.p2 = p2;
+    }
+
+    equals(segment) {
+        return this.includes(segment.p1) && this.includes(segment.p2);
+    }
+
+    includes(point) {
+        return this.p1.equals(point) || this.p2.equals(point);
     }
 
     draw(ctx, width = 2, color = 'black') {
